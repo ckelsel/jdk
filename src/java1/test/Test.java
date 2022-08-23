9c5deeec64d4f9fbef70a4c772cd7532e80d3c88
xkm <ckelsel@hotmail.com>
2022-08-23 10:24:51 +0800

    feat: java version 1.8.0_311

---
+++ src/java1/test/Test.java
@@ -0,0 +1,12 @@
+package java1.test;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(1, 1);
+    }
+
+}
